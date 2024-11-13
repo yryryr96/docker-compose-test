3cbf5240ac62d2e0f22a5e53248dc0937f0f45bd
정영록 <s9285s@naver.com>
2024-11-14 02:02:04 +0900

    init compose-test-repository

---
+++ server2/src/main/java/com/example/demo/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String check() {
+
+        return "server2 is running";
+    }
+}
