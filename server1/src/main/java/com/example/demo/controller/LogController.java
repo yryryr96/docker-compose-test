691c002616d143518a3e3cf82fa964dddac2fc51
정영록 <s9285s@naver.com>
2024-11-17 21:52:15 +0900

    Add logger-apply

---
+++ server1/src/main/java/com/example/demo/controller/LogController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LogController {
+
+    @GetMapping("/log")
+    public String getLogMessage() {
+
+        log.error("error log");
+        log.warn("warn log");
+        log.info("info log");
+        log.debug("debug log");
+        log.trace("trace log");
+
+        return "log controller success";
+    }
+}
