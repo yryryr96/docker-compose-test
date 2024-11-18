e31535c230ddff112dd7b988a8230ee387759f19
정영록 <s9285s@naver.com>
2024-11-18 22:17:57 +0900

    modify dockerfile

---
+++ server1/src/test/java/com/example/demo/controller/HealthControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void healthControllerTest() throws Exception {
+        //given
+        //when
+        ResultActions result = mockMvc.perform(get("/health"));
+        //then
+        MvcResult mvcResult = result.andExpect(status().isOk())
+                .andExpect(content().string("server1 is running"))
+                .andReturn();
+
+    }
+}
