f09f277c8d7a1f830bd6831f577349f57822cc9c
정영록 <s9285s@naver.com>
2024-11-21 01:42:56 +0900

    delete ActiveProfiles

--- server1/src/test/java/com/example/demo/controller/HealthControllerTest.java
+++ server1/src/test/java/com/example/demo/controller/HealthControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 class HealthControllerTest {
 
     @Autowired
