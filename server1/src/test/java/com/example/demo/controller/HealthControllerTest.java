fb4586ca94db6b55593f29d2d25fff70e0639fa3
정영록 <s9285s@naver.com>
2024-11-27 04:46:50 +0900

    Add change pakage structure

--- server1/src/test/java/com/example/demo/controller/HealthControllerTest.java
+++ server1/src/test/java/com/example/demo/controller/HealthControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,16 +13,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 class HealthControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    Environment env;
 
     @Test
     void healthControllerTest() throws Exception {
@@ -30,11 +35,10 @@ class HealthControllerTest {
         //when
         ResultActions result = mockMvc.perform(get("/health"));
         //then
-//        MvcResult mvcResult = result.andExpect(status().isOk())
-//                .andExpect(content().string("server1 is running!!!"))
-//                .andReturn();
+        MvcResult mvcResult = result.andExpect(status().isOk())
+                .andExpect(content().string("server1 is running!!!"))
+                .andReturn();
 
-//        assertThat(env.getActiveProfiles()).contains("test");
-//        assertThat(username).isEqualTo("test_username");
+        assertThat(env.getActiveProfiles()).contains("test");
     }
 }
